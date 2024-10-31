1ab930ae2f9703fe5dae6e2152ea9457d39faa61
v-zijiagu <v-zijiagu@microsoft.com>
2024-10-31 17:00:10 +0800

    新增用代码修改.NET程序集IL指令

---
+++ DotNetWait/DotNetWait/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace DotNetWait;
+
+public class Options
+{
+    [Option('f',"file",Required = true, HelpText = "the filename to be modified")]
+    public string File { get; set; }
+    [Option('m',"mode", Required = false, Default = Mode.Break, HelpText = "Prompt: Prompt the user to attach the application to a debugger; Break: launch JIT debugger(Windows only).")]
+    public Mode Mode { get; set; }
+
+}
+
+public enum Mode
+{
+    Prompt,
+    Break
+}
---
+++ DotNetWait/DotNetWait/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using CommandLine;
+using DotNetWait;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.Diagnostics;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(o =>
+    {
+        Run(o);
+    });
+
+void Run(Options options)
+{
+    // Load the assembly:must can write
+    using var assembly = AssemblyDefinition.ReadAssembly(options.File, new ReaderParameters { ReadWrite = true});
+
+    // Find the entry point method
+    var entryPoint = assembly.EntryPoint;
+
+    // Get the IL processor for the entry point method
+    var ilProcessor = entryPoint.Body.GetILProcessor();
+
+    // Create instructions to add
+    Instruction[] instructions;
+    if (options.Mode == Mode.Break)
+    {
+        var lanchDebuggerInstruction = ilProcessor.Create(OpCodes.Call, assembly.MainModule.ImportReference(typeof(Debugger).GetMethod(nameof(Debugger.Launch), Type.EmptyTypes)));
+        instructions = new[]
+        { 
+            lanchDebuggerInstruction,
+            Instruction.Create(OpCodes.Pop),
+        };
+    }
+    else if (options.Mode == Mode.Prompt)
+    {
+        var writeLineInstruction = ilProcessor.Create(OpCodes.Call, assembly.MainModule.ImportReference(typeof(Console).GetMethod(nameof(Console.WriteLine), new Type[] { typeof(string )})));
+        var readKeyInstruction = ilProcessor.Create(OpCodes.Call, assembly.MainModule.ImportReference(typeof(Console).GetMethod(nameof(Console.ReadKey), Type.EmptyTypes)));
+
+        instructions = new[]
+        {
+            Instruction.Create(OpCodes.Ldstr, "Please attach debugger, then press any key"),
+            writeLineInstruction,
+            readKeyInstruction,
+            Instruction.Create(OpCodes.Pop),// Use Pop to remove the return value of Readkey function
+        };
+    }
+    else
+    {
+        throw new InvalidOperationException("Invalid mode");
+    }
+
+    var firstInstruction = ilProcessor.Body.Instructions[0];
+    // Insert the new instructions at the beginning of the method
+    for (int i = 0; i < instructions.Length; i++)
+    {
+        ilProcessor.InsertBefore(firstInstruction, instructions[i]);
+    }
+
+    // Save the modified assembly
+    assembly.Write();
+
+    Console.WriteLine("Assembly modified and saved successfully");
+}
